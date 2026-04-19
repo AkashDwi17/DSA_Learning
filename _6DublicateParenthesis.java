89a87f1315b1021408ceae7bd5db12d7827a58fb
Akash Dwivedi <akashdwi17@gmail.com>
2026-04-19 09:06:56 +0530

    Dublicate Parenthesis

---
+++ _6DublicateParenthesis.java
@@ -0,0 +1,32 @@
+package Stack;
+import java.util.*;
+
+public class _6DublicateParenthesis {
+    public static boolean dublicateParenthesis (String str){
+        Stack <Character> s = new Stack<>();
+        for (int i=0; i<str.length(); i++){
+            char ch = str.charAt(i);
+
+            if (ch == ')'){
+                int count = 0;
+                while (!s.isEmpty() && s.peek() != '('){
+                    s.pop();
+                    count ++;
+                }
+                if (count < 1){
+                    return true;
+                }else{
+                    s.pop();
+                }
+            }
+            else {
+                s.push(ch);
+            }
+        }
+        return false;
+    } 
+   public static void main (String args[]){
+    String str = "(((())))";  // ((a+b)*(c+d))
+    System.out.println(dublicateParenthesis(str));
+   }
+}
