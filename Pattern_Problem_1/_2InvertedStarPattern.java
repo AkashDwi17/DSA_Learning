8e08e9c06664442714a64e23b451bdd34e6bcb06
Akash Dwivedi <akashdwi17@gmail.com>
2026-04-23 07:39:15 +0530

    Inverted Star Pattern

---
+++ Pattern_Problem_1/_2InvertedStarPattern.java
@@ -0,0 +1,14 @@
+package Pattern_Problem_1;
+
+public class _2InvertedStarPattern {
+    public static void main (String args[]){
+        int n = 5;
+        for (int i=1; i<=n; i++){
+            for (int j=1; j<=n-i+1; j++){
+                System.out.print ("* ");
+            }
+            System.out.println ();
+        }
+    }
+
+}
